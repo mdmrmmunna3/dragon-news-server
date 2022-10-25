bce25c6ae4331cb411bf38e955866ec1ccd32327
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-25 12:48:58 +0600

    server side deploy successfully

--- index.js
+++ index.js
@@ -7,12 +7,43 @@ app.use(cors());
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json');
+
 app.get('/', (req, res) => {
     res.send('News API Running!')
 })
 
+// categories data
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoriesNews = news.filter(n => n.category_id === id);
+        res.send(categoriesNews);
+    }
+
+})
+
+
+// news data with dynamically
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    // using find...
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+
+app.get('/news', (req, res) => {
+    res.send(news)
 })
 
 app.listen(port, () => {
